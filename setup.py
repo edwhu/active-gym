9a9e73181fa95810f899c1743a046f2066d6262c
Edward Hu <husedward@gmail.com>
2025-02-12 19:12:56 -0600

    deps

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     keywords='active-reinforcement-learning active-rl reinforcement learning rl gym atari deepmind robosuite',
     packages=find_packages(),
     install_requires=[
-        "mujoco<3.0",
+        "mujoco",
         "mujoco-py>=2.1.2.14",
         "atari-py",
-        "dm-control==1.0.11",
+        "dm-control",
         # "gymnasium>=0.28.1,<1.0.0",
         "torch",
         "torchvision",
